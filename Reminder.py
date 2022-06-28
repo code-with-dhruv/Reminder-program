821db329a4e39e914ddddd68c68ce0750956a81e
dhruv <87694090+code-with-dhruv@users.noreply.github.com>
2022-06-29 00:43:05 +0530

    final script

--- Reminder.py
+++ Reminder.py
@@ -1,1 +1,18 @@
 
+from email import message
+from socket import timeout
+import time
+from plyer import notification
+
+
+if __name__=="__main__":
+    while True:
+        a=input("Enter a task--->")
+        b=input("Enter Task description--->")
+        ti=int(input("Enter the duration of the task in in minutes--->"))
+        notification.notify(
+            title="Reminder to do-{}".format(a),
+            message="{}".format(b),
+            timeout=20)
+        time.sleep(ti*60)
+
